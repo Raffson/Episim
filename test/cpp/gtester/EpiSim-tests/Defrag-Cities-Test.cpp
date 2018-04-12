2b0a6feda4e4c87fa83291f03592e2510ea3a2a9
RobbeHeirman <robbe.heirman@student.uantwerpen.be>
2018-04-12 14:55:26 +0200

    -Finished Defrag Test - Debugged Defragging

--- test/cpp/gtester/EpiSim-tests/Defrag-Cities-Test.cpp
+++ test/cpp/gtester/EpiSim-tests/Defrag-Cities-Test.cpp
@@ -5,10 +5,8 @@
 #include <vector>
 #include "popgen-Episim/GeoGen/GeoGrid.h"
 
-#include <boost/property_tree/ptree.hpp>
 #include <gtest/gtest.h>
-#include <omp.h>
-#include <spdlog/spdlog.h>
+
 
 namespace Tests {
 
@@ -20,29 +18,57 @@ namespace Tests {
 
     class DefragCityTest : public ::testing::TestWithParam<unsigned int> {
     public:
-        /// TestCase set up.
-        static void SetUpTestCase() {}
-
-        /// Tearing down TestCase
-        static void TearDownTestCase() {}
+        DefragCityTest() = default;
 
     protected:
+        GeoGrid* geo{};
         /// Destructor has to be virtual.
         ~DefragCityTest() override = default;
 
         /// Set up for the test fixture
-        void SetUp() override {}
+        void SetUp() override {
+        geo = new GeoGrid("config/geogen_default.xml");
+        }
 
         /// Tearing down the test fixture
-        void TearDown() override {}
+        void TearDown() override {
+                    delete geo;
+        }
     };
 
     TEST_P(DefragCityTest, happy_defrag) {
+        double X = 0.5;
+        double Y = 0.001; // should be 32 cities
+        vector<double> p_vec = {1};
 
-        double X = 0.1;
-        double Y = 0.5;
-        vector<double> p_vec = {0,1};
+        long counter = 0;
+        double pop_treshold = X * geo->GetTotalPop();
+        geo->GenerateAll();
+        for(auto &it: geo->GetCities()){ //327 cities
+
+            if (pop_treshold > it.second.GetPopulation()){
+                counter++;
+            }
+        }
+
+        geo->DefragmentSmallestCities(X, Y, p_vec);
+
+        EXPECT_EQ(343, geo->GetCities().size());
+
+
     }
+
+    namespace {
+// OpenMP should have no effect atm...
+    #ifdef _OPENMP
+            unsigned int threads[]{1U, 4U};
+    #else
+            unsigned int threads[]{1U};
+    #endif
+
+    } // namespace
+
+    INSTANTIATE_TEST_CASE_P(Run, DefragCityTest,ValuesIn(threads));
 }// Namespace Test
 
 
