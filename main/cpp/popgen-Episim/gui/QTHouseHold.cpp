70db4acfe5c514b07829f0160efc1e84d9f98d66
RobbeHeirman <robbeheirman@msn.com>
2018-06-19 08:50:34 +0200

    QTHousehold to represent households

---
+++ main/cpp/popgen-Episim/gui/QTHouseHold.cpp
@@ -0,0 +1,5 @@
+//
+// Created by robbe on 19.06.18.
+//
+
+#include "QTHouseHold.h"
---
+++ main/cpp/popgen-Episim/gui/QTHouseHold.h
@@ -0,0 +1,14 @@
+//
+// Created by robbe on 19.06.18.
+//
+
+#ifndef STRIDE_QTHOUSEHOLD_H
+#define STRIDE_QTHOUSEHOLD_H
+
+
+class QTHouseHold {
+
+};
+
+
+#endif //STRIDE_QTHOUSEHOLD_H
