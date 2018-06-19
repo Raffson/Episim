18fbea0f41295b80851a38ecb4514806e148a50a
Nishchal Shrestha <nishrestha_68@hotmail.com>
2018-06-19 13:13:39 +0200

    documentation added

--- main/cpp/popgen-Episim/gui/QTPopulation.h
+++ main/cpp/popgen-Episim/gui/QTPopulation.h
@@ -24,25 +24,35 @@ public:
     void VisualiseAll();
 
 public slots:
+    /// Displays bardiagrams for age distribution
     void ageDistributionClicked();
+
+    /// Displays piechart for population density
     void populationDensityClicked();
+
+    /// Notify if workplace distribution is clicked and handles appropriately
     void workplaceClicked();
+
+    /// Notify if household distribution is clicked and handles appropriately
     void householdClicked();
 
 private:
+    /// Displays bardiagram
+    /// @param toManage if workplace then displays the bardiagram of different types of workplaces
+    /// otherwise of different types of households
     void manage(std::string toManage);
 
 private:
-    QMainWindow* m_window;
-    QWidget* m_wdg;
-    QVBoxLayout *m_vlay;
-    QtCharts::QChartView *m_chartView;
-    std::map <std::string, unsigned int>m_pop_counter;
-    std::map <unsigned int, unsigned int>m_household_counter;
-    std::map <std::string, unsigned int>m_workplace_counter;
+    QMainWindow* m_window; ///< The window where charts will appear
+    QWidget* m_wdg; ///< the widget
+    QVBoxLayout *m_vlay; ///< This will align widgets vertically
+    QtCharts::QChartView *m_chartView; ///< the chartview
 
-
-
+    std::map <std::string, unsigned int>m_pop_counter; ///< holds the counts of different types of population based on age
+    std::map <unsigned int, unsigned int>m_household_counter; ///< holds the number of households with the key describing
+                                                                        //the number members in the household
+    std::map <std::string, unsigned int>m_workplace_counter; ///< counter for different types of workplaces
+    
 };
 
 
