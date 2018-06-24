d5bf76e4de620927fc0a66f8cbf3bc6185ffa217
RobbeHeirman <robbe.heirman@student.uantwerpen.be>
2018-06-24 23:13:43 +0200

    Reading data with papi

--- main/cpp/Papi.cpp
+++ main/cpp/Papi.cpp
@@ -13,57 +13,71 @@
 #include <sys/types.h>
 #include <memory.h>
 #include <malloc.h>
+#include <vector>
+
 #include "papi.h"
+#include "omp.h"
 
 #include "popgen-Episim/generator/GeoGridGenerator.h"
 #include "popgen-Episim/generator/PopulationGenerator.h"
-#include "util/InstallDirs.h"
+#include "util/FileSys.h"
+#include "sim/SimRunner.h"
 
-static void test_fail(char *file, int line, char *call, int retval);
+using namespace std;
 
-int main(int argc, char **argv) {
-    extern void dummy(void *);
-    float real_time, proc_time, mflops;
-    long long flpins;
-    int retval;
-    int i,j,k;
+int main() {
+    boost::property_tree::ptree pt = stride::util::FileSys::ReadPtreeFile(
+            string("../config/run_default.xml"));
+
+    boost::property_tree::ptree pt_geo = stride::util::FileSys::ReadPtreeFile(
+            string("../config/geogen_default.xml"));
+
+    string base_command = "mkdir -p mem_data" ;
+    string base = "mem_data";
+    system(base_command.c_str());
+    vector<string> rngs{"yarn2"}; // mrg3
+
+    PAPI_dmem_info_t dmem;
+    PAPI_library_init(PAPI_VER_CURRENT);
+    PAPI_thread_init(reinterpret_cast<unsigned long (*)(void)>(omp_get_thread_num));
+
+    unsigned long pop = 1000000; // Test cases
+        for (auto &rng: rngs){
+            pt.put("run.rng_type", rng);
+            cout << "Starting " << rng << endl;
+            for (unsigned threads = 3; threads <= 3; threads++) {
+                cout << "At thread " << to_string(threads) << endl;
+                pt.put("run.num_threads", threads);
+                string dir = base_command + "/" + rng + "/" + to_string(threads);
+                system(dir.c_str());
+
+                ofstream myfile;
+                string filename = base + "/" + rng + "_" + to_string(threads)+ ".dat";
+                myfile.open (filename);
+
+                for (unsigned i = 0; i < 10; i++) { // Pop loop
+
+                    cout << "With pop count " << to_string((i + 1) * pop) << endl;
+                    pt_geo.put("pop_info.pop_total", (i + 1) * pop);
 
 
-    /* Setup PAPI library and begin collecting data from the counters */
-    if((retval=PAPI_flops( &real_time, &proc_time, &flpins, &mflops))<PAPI_OK)
-        test_fail(__FILE__, __LINE__, "PAPI_flops", retval);
 
 
-    stride::GeoGridGenerator gen;
-    auto grid = gen.Generate("../config/run_default.xml");
-    stride::PopulationGenerator pop(*grid);
-    pop.Generate();
-    
-    /* Collect the data into the variables passed in */
-    if((retval=PAPI_flops( &real_time, &proc_time, &flpins, &mflops))<PAPI_OK)
-        test_fail(__FILE__, __LINE__, "PAPI_flops", retval);
+                    stride::GeoGridGenerator geo_gen;
+                    auto grid = geo_gen.Generate(pt);
+                    stride::PopulationGenerator pop_gen(*grid);
+                    pop_gen.Generate();
+                    stride::SimRunner rn(pt, grid->GetPopulation());
+                    rn.Run();
 
-    printf("Real_time:\t%f\nProc_time:\t%f\nTotal flpins:\t%lld\nMFLOPS:\t\t%f\n",
-           real_time, proc_time, flpins, mflops);
-    printf("%s\tPASSED\n", __FILE__);
-    PAPI_shutdown();
-    exit(0);
-}
+                    PAPI_get_dmem_info(&dmem);
+                    printf("Mem Size:\t\t%lld\n", dmem.size);
+                    myfile << to_string((i + 1) * pop) << " " << to_string(dmem.size) << endl ;
 
-static void test_fail(char *file, int line, char *call, int retval){
-    printf("%s\tFAILED\nLine # %d\n", file, line);
-    if ( retval == PAPI_ESYS ) {
-        char buf[128];
-        memset( buf, '\0', sizeof(buf) );
-        sprintf(buf, "System error in %s:", call );
-        perror(buf);
+                    ;
+                }
+                myfile.close();
+            }
+            PAPI_shutdown();
+        }
     }
-    else if ( retval > 0 ) {
-        printf("Error calculating: %s\n", call );
-    }
-    else {
-        printf("Error in %s: %s\n", call, PAPI_strerror(retval) );
-    }
-    printf("\n");
-    exit(1);
-}
